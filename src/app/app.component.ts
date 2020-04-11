71c34e6e6769bf2ce2ee11ba0b8dd0fe9b4f3b42
Kumar Priyansh <bobbyskhs@gmail.com>
2020-04-11 19:48:02 +0530

    Added orientation lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { AlertController } from '@ionic/angular';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +17,27 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private nativeStorage: NativeStorage
+    private nativeStorage: NativeStorage,
+    public alertController: AlertController,
+    private screenOrientation: ScreenOrientation
   ) {
     this.initializeApp();
+    this.lockScreenOrientation();
+  }
+
+  lockScreenOrientation() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+  }
+
+  async noInternetAlert() {
+    const alert = await this.alertController.create({
+      header: 'No Internet Connection',
+      // tslint:disable-next-line: max-line-length
+      message: 'The app needs to be consistently connected to the internet to fetch live status. Please check your internet connection and try again.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
   initializeApp() {
