59d7787bbe556ef04f49b42fc89120335e28dd5e
Rohit <rohitsingh11121996@gmail.com>
2020-04-11 23:42:25 +0530

    table

--- src/app/state-data/state-data.component.ts
+++ src/app/state-data/state-data.component.ts
@@ -8,45 +8,7 @@ import * as $ from "jquery";
 })
 export class StateDataComponent implements OnInit {
   stateData = [];
-  colors = [
-    { id: 1, color: "#085582" },
-    { id: 2, color: "#1c3b4d" },
-    { id: 3, color: "#2b2727" },
-    { id: 4, color: "#2b0406" },
-    { id: 5, color: "#e4ebc7" },
-    { id: 6, color: "#d1ce08" },
-    { id: 7, color: "#d18b08" },
-    { id: 8, color: "#d16d08" },
-    { id: 9, color: "#08c7d1" },
-    { id: 10, color: "#0881d1" },
-    { id: 11, color: "#7308d1" },
-    { id: 12, color: "#d108c4" },
-    { id: 13, color: "#e4ebc7" },
-    { id: 14, color: "#d1080b" },
-    { id: 15, color: "#121010" },
-    { id: 16, color: "#332020" },
-    { id: 17, color: "#333120" },
-    { id: 18, color: "#203333" },
-    { id: 19, color: "#202833" },
-    { id: 20, color: "#202890" },
-    { id: 21, color: "#538a5f" },
-    { id: 22, color: "#4c59ad" },
-    { id: 23, color: "#e3849d" },
-    { id: 24, color: "#b784e3" },
-    { id: 25, color: "#9ce384" },
-    { id: 26, color: "#e3ca84" },
-    { id: 27, color: "#f55f02" },
-    { id: 28, color: "#f5ed02" },
-    { id: 29, color: "#c0f502" },
-    { id: 30, color: "#02edf5" },
-    { id: 31, color: "#0212f5" },
-    { id: 32, color: "#9002f5" },
-    { id: 33, color: "#f502d1" },
-    { id: 34, color: "#f5026f" },
-    { id: 35, color: "#02a8f5" },
-    { id: 36, color: "#61604e" },
-    { id: 37, color: "#514e61" },
-  ];
+  images = [];
   constructor() {}
 
   ngOnInit() {
@@ -65,19 +27,49 @@ export class StateDataComponent implements OnInit {
   updataUI() {
     // $("#state_data_card").empty();
     for (let i = 1; i < this.stateData.length; i++) {
-      console.log(this.stateData[i]);
       $("#state_data_card").append(`<ion-slide>
       <div class="slide">
-        <ion-card style='background-color: ${this.colors[i - 1].color};'>
+        <ion-card >
           
           <ion-card-header>
-            <ion-card-subtitle>+${
+            <ion-card-subtitle style='color: red;'>+${
               this.stateData[i].deltaconfirmed
             } Today</ion-card-subtitle>
             <ion-card-title>${this.stateData[i].state}</ion-card-title>
           </ion-card-header>
           <ion-card-content>
-          
+           <div style="width: 100%;">
+            <ion-row>
+             <ion-col>
+             <div style='color: red;'>Affected</div>
+             <div style='color: red;'>${this.stateData[i].confirmed}</div>
+             <div style='color: red;'>[${
+               this.stateData[i].deltaconfirmed
+             }]</div>
+             </ion-col>
+             <ion-col>
+             <div style='color: blue;'>Active</div>
+             <div style='color: blue;'>${this.stateData[i].active}</div>
+             <div style='color: blue;'>[${
+               parseInt(this.stateData[i].deltaconfirmed) -
+               (parseInt(this.stateData[i].deltarecovered) +
+                 parseInt(this.stateData[i].deltadeaths))
+             }]</div>
+             </ion-col>
+             <ion-col>
+             <div style='color: green;'>Recovered</div>
+             <div style='color: green;'>${this.stateData[i].recovered}</div>
+             <div style='color: green;'>[${
+               this.stateData[i].deltarecovered
+             }]</div>
+             </ion-col>
+             <ion-col>
+             <div style='color: black;'>Deceased</div>
+             <div style='color: black;'>${this.stateData[i].deaths}</div>
+             <div style='color: black;'>[${this.stateData[i].deltadeaths}]</div>
+             </ion-col>
+            </ion-row>
+           </div>
           </ion-card-content>
         </ion-card>
       </div>
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  countryData = [];
 
   constructor() {}
 
+  ngOnInit() {
+    this.loadCountryData();
+  }
+  loadCountryData() {
+    $.get(
+      "https://corona.lmao.ninja/countries?sort=country",
+      (data, status) => {
+        this.setCountryData(data);
+      }
+    );
+  }
+  setCountryData(data) {
+    this.countryData = data;
+  }
+  // updateUI() {
+  //   $("#table_data").empty();
+  //   for (let i = 0; i < this.countryData.length - 1; i++) {
+  //     $("#table_data").append(`
+  //     <tr style='border: solid 1px #000;'>
+  //        <td style='border: solid 1px #000;>CompanyC</td>
+  //        <td style='border: solid 1px #000;>0</td>
+  //        <td style='border: solid 1px #000;>7</td>
+  //        <td style='border: solid 1px #000;>2</td>
+  //        <td style='border: solid 1px #000;>0</td>
+  //   </tr>
+  //   `);
+  //   }
+  // }
 }
+// <ion-row>
+//     <ion-col style="">
+//     <img style="width: 50%;" src='${this.countryData[i].countryInfo.flag}'>
+//     </img></ion-col>
+//     <ion-col
+//       style="color: black; font-size: 12px;"
+//       >${this.countryData[i].country}</ion-col
+//     >
+//     <ion-col
+//       style="background-color: #501717; color: white; border: 1px solid gray;"
+//       >
+//       <div>${this.countryData[i].cases}</div>
+//      <div> +[${this.countryData[i].todayCases}]</div>
+//      </ion-col
+//     >
+//     <ion-col
+//       style="background-color: #23308e; color: white; border: 1px solid gray;"
+//       >
+//       <div>${this.countryData[i].active}</div>
+//       </ion-col
+//     >
+//     <ion-col
+//       style="background-color: #238e43; color: white; border: 1px solid gray;"
+//       >
+//       <div>${this.countryData[i].recovered}</div>
+//       </ion-col
+//     >
+//     <ion-col
+//       style="background-color: #2f2f2f; color: white; border: 1px solid gray;"
+//       >
+//       <div>${this.countryData[i].deaths}</div>
+//       <div>+[${this.countryData[i].todayDeaths}]</div>
+//       </ion-col
+//     >
+
+//     </ion-row>
