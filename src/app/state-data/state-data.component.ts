417814f6eca57fad61b14719c70e34706fee3e34
Kumar Priyansh <bobbyskhs@gmail.com>
2020-04-11 14:12:03 +0530

    feat: state wise data slider

--- src/app/state-data/state-data.component.ts
+++ src/app/state-data/state-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-state-data',
@@ -9,6 +10,8 @@ export class StateDataComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }
