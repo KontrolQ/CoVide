a8c86dc6f544214dc486173e68f2234a46457e70
Rohit <rohitsingh11121996@gmail.com>
2020-04-11 06:13:16 +0530

    add box and button

---
+++ src/app/state-data/state-data.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-state-data',
+  templateUrl: './state-data.component.html',
+  styleUrls: ['./state-data.component.scss'],
+})
+export class StateDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,10 +1,10 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Tab1Page } from "./tab1.page";
+import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 
 @NgModule({
   imports: [
@@ -12,8 +12,8 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: "", component: Tab1Page }]),
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
