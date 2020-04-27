fe5ba7145e54a4f5f49e8445a243f882044c504d
Kumar Priyansh <bobbyskhs@gmail.com>
2020-04-27 18:27:58 +0530

    Added bg to top bar on tab 1 page, added graphs to world data, sorted world data, added status bar by hex color

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import * as $ from "jquery";
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 import {
   NavController,
   AlertController,
   ModalController,
-} from "@ionic/angular";
-import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
-import * as Chart from "chart.js";
-import { NotificationsModalPage } from "../notifications-modal/notifications-modal.page";
+} from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import * as Chart from 'chart.js';
+import { NotificationsModalPage } from '../notifications-modal/notifications-modal.page';
 
 @Component({
-  selector: "app-tab1",
-  templateUrl: "tab1.page.html",
-  styleUrls: ["tab1.page.scss"],
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
   major = 0;
@@ -42,7 +42,7 @@ export class Tab1Page implements OnInit {
   }
 
   getRecentData() {
-    $.get("https://api.covid19india.org/data.json", (data) => {
+    $.get('https://api.covid19india.org/data.json', (data) => {
       this.setRecentData(data);
     });
   }
@@ -68,42 +68,42 @@ export class Tab1Page implements OnInit {
     for (let i = this.recentData.length - 10; i < this.recentData.length; i++) {
       labelsData.push(this.recentData[i].date);
     }
-    const canvas = document.getElementById("history_data") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById('history_data') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
     const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, "rgba(58,72,237,0.3)");
-    gradient.addColorStop(1, "rgba(255,255,255,1)");
+    gradient.addColorStop(0, 'rgba(58,72,237,0.3)');
+    gradient.addColorStop(1, 'rgba(255,255,255,1)');
     const myChart = new Chart(ctx, {
-      type: "bar",
+      type: 'bar',
       data: {
         labels: labelsData,
         datasets: [
           {
-            label: "Total Reported Cases",
+            label: 'Total Reported Cases',
             data: datasetData,
             backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-              "rgba(131, 255, 64, 0.2)",
-              "rgba(64, 223, 255, 0.2)",
-              "rgba(255, 64, 195, 0.2)",
-              "rgba(179, 71, 21, 0.2)",
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(131, 255, 64, 0.2)',
+              'rgba(64, 223, 255, 0.2)',
+              'rgba(255, 64, 195, 0.2)',
+              'rgba(179, 71, 21, 0.2)',
             ],
             borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-              "rgba(131, 255, 64, 1)",
-              "rgba(64, 223, 255, 1)",
-              "rgba(255, 64, 195, 1)",
-              "rgba(179, 71, 21, 1)",
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(131, 255, 64, 1)',
+              'rgba(64, 223, 255, 1)',
+              'rgba(255, 64, 195, 1)',
+              'rgba(179, 71, 21, 1)',
             ],
             borderWidth: 1,
           },
@@ -117,7 +117,7 @@ export class Tab1Page implements OnInit {
         scales: {
           animation: {
             duration: 2000,
-            easing: "easeOutQuart",
+            easing: 'easeOutQuart',
           },
           xAxes: [
             {
@@ -146,17 +146,17 @@ export class Tab1Page implements OnInit {
 
   async updateApp() {
     const alert = await this.alertController.create({
-      header: "App Update Available!",
+      header: 'App Update Available!',
       message:
-        "We will take you to our website. Download the app and install it manually.!",
+        'We will take you to our website. Download the app and install it manually.!',
       buttons: [
         {
-          text: "Later",
-          role: "cancel",
-          cssClass: "secondary",
+          text: 'Later',
+          role: 'cancel',
+          cssClass: 'secondary',
         },
         {
-          text: "Okay",
+          text: 'Okay',
           handler: () => {
             this.getUpdate();
           },
@@ -174,18 +174,18 @@ export class Tab1Page implements OnInit {
       this.showNotification = false;
     }
     this.showNotification = true;
-    $.get("https://api.covid19india.org/updatelog/log.json", (data) => {
+    $.get('https://api.covid19india.org/updatelog/log.json', (data) => {
       this.updates = data;
     });
-    console.log("hi");
+    console.log('hi');
   }
   getUpdate() {
-    window.open("https://theorangecoffeeproject.github.io/CoVide/", "_system");
+    window.open('https://theorangecoffeeproject.github.io/CoVide/', '_system');
   }
 
   checkUpdate() {
     $.get(
-      "https://raw.githubusercontent.com/TheOrangeCoffeeProject/CoVide/master/updateData.json",
+      'https://raw.githubusercontent.com/TheOrangeCoffeeProject/CoVide/master/updateData.json',
       (data) => {
         const updateData = JSON.parse(data);
         if (
@@ -212,21 +212,21 @@ export class Tab1Page implements OnInit {
   }
   loadData() {
     let disabled = false;
-    $.get("https://api.covid19india.org/data.json", (data, status) => {
-      $("#total_affected").text(data.statewise[0].confirmed);
-      $("#active_data").text(data.statewise[0].active);
-      $("#recovered_data").text(data.statewise[0].recovered);
-      $("#deceased_data").text(data.statewise[0].deaths);
-      $("#daily_affected").text(data.statewise[0].deltaconfirmed);
-      $("#daily_recovered").text(data.statewise[0].deltarecovered);
-      $("#daily_death").text(data.statewise[0].deltadeaths);
-      $("#daily_active").text(
+    $.get('https://api.covid19india.org/data.json', (data, status) => {
+      $('#total_affected').text(data.statewise[0].confirmed);
+      $('#active_data').text(data.statewise[0].active);
+      $('#recovered_data').text(data.statewise[0].recovered);
+      $('#deceased_data').text(data.statewise[0].deaths);
+      $('#daily_affected').text(data.statewise[0].deltaconfirmed);
+      $('#daily_recovered').text(data.statewise[0].deltarecovered);
+      $('#daily_death').text(data.statewise[0].deltadeaths);
+      $('#daily_active').text(
         // tslint:disable: radix
         parseInt(data.statewise[0].deltaconfirmed) -
           (parseInt(data.statewise[0].deltarecovered) +
             parseInt(data.statewise[0].deltadeaths))
       );
-      $("#last_update").text(data.statewise[0].lastupdatedtime);
+      $('#last_update').text(data.statewise[0].lastupdatedtime);
       disabled = false;
     });
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import * as $ from "jquery";
-import { LoadingController, AlertController } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as Chart from 'chart.js';
+import { LoadingController, AlertController } from '@ionic/angular';
 @Component({
-  selector: "app-tab2",
-  templateUrl: "tab2.page.html",
-  styleUrls: ["tab2.page.scss"],
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
   countryData = [];
   isLoading = false;
 
   constructor(
     public loadingController: LoadingController,
     public alertController: AlertController
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.presentLoading();
@@ -23,8 +24,8 @@ export class Tab2Page {
     this.isLoading = true;
     return await this.loadingController
       .create({
-        message: "Fetching Data...",
-        mode: "ios",
+        message: 'Fetching Data...',
+        mode: 'ios',
       })
       .then((a) => {
         a.present().then(() => {
@@ -36,7 +37,7 @@ export class Tab2Page {
   }
 
   loadCountryData() {
-    $.get("https://api.covid19api.com/summary", (data, status) => {
+    $.get('https://api.covid19api.com/summary', (data, status) => {
       // console.log(data.Countries);
       this.dismissLoading();
       this.setCountryData(data.Countries);
@@ -47,56 +48,163 @@ export class Tab2Page {
     return await this.loadingController.dismiss();
   }
   setCountryData(data) {
+    const sortBy = (field, reverse, primer) => {
+
+      const key = primer ?
+        // tslint:disable: only-arrow-functions
+        function (x) {
+          return primer(x[field]);
+        } :
+        function (x) {
+          return x[field];
+        };
+
+      reverse = !reverse ? 1 : -1;
+
+      return function (a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+      };
+    };
     this.countryData = data;
+    this.countryData = this.countryData.sort(sortBy('TotalConfirmed', true, parseInt));
     this.updateUI();
   }
   updateUI() {
-    $("#World_card").empty();
+    // tslint:disable: max-line-length
+    this.loadPieChart(this.countryData[0]);
+    $('#World_card').empty();
     for (let i = 0; i < this.countryData.length - 1; i++) {
-      $("#World_card").append(`
-       <ion-card>
-       <ion-card-header>
-       <ion-card-subtitle style="color: red;">+${
-         this.countryData[i].NewConfirmed
-       } Today</ion-card-subtitle>
-       <ion-card-title style='font-weight: bold;'>${
-         this.countryData[i].Country
-       }</ion-card-title>
-     </ion-card-header>
-     <ion-card-content>
-       <ion-row>
-       <ion-col style='color: #501717; text-align:center;'>
-        <div>Affected</div>
-        <div >${this.countryData[i].TotalConfirmed}</div>
-        <div style='font-size: 11px;'>+ [${
-          this.countryData[i].NewConfirmed
-        }]</div>
-       </ion-col>
-       <ion-col style='text-align: center; color: #23308e;'>
-       <div >Active</div>
-       <div >${
-         parseInt(this.countryData[i].TotalConfirmed) -
-         (parseInt(this.countryData[i].TotalRecovered) +
-           parseInt(this.countryData[i].TotalDeaths))
-       }</div>
-       </ion-col>
-       <ion-col style='text-align: center; color: #238e43;'>
-       <div >Recovered</div>
+      $('#World_card').append(`
+      <div id="${i}" class="loadingPieChatData" style="background-color: #f2f2f2;display: inline-block; padding: 15px; border-radius: 12px; width: 85%; height: 115px; margin: 0px 10px 0px 0px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      box-shadow: 1px 5px 20px 2px rgba(0, 0, 0, 0.06);">
+       <div style="  display: grid; grid-template-columns: auto; grid-gap: 10px;">
+        <div id="${i}" style="text-align: center;  white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;">
+        <br>
+            <div style='color: red; font-size: 15px;'>+${this.countryData[i].NewConfirmed} Today</div>
+            <div>
+            <ion-row style=" white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;">
+            <ion-col style="font-size: 20px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${this.countryData[i].Country}</ion-col>
+            </ion-row>
+            </div>
+        </div>
+        </div>
+      </div>`);
+      //     $('#World_card').append(`
+      //      <ion-card>
+      //      <ion-card-header>
+      //      <ion-card-subtitle style="color: red;">+${
+      //       this.countryData[i].NewConfirmed
+      //       } Today</ion-card-subtitle>
+      //      <ion-card-title style='font-weight: bold;'>${
+      //       this.countryData[i].Country
+      //       }</ion-card-title>
+      //    </ion-card-header>
+      //    <ion-card-content>
+      //      <ion-row>
+      //      <ion-col style='color: #501717; text-align:center;'>
+      //       <div>Affected</div>
+      //       <div >${this.countryData[i].TotalConfirmed}</div>
+      //       <div style='font-size: 11px;'>+ [${
+      //       this.countryData[i].NewConfirmed
+      //       }]</div>
+      //      </ion-col>
+      //      <ion-col style='text-align: center; color: #23308e;'>
+      //      <div >Active</div>
+      //      <div >${
+      //       // tslint:disable: radix
+      //       parseInt(this.countryData[i].TotalConfirmed) -
+      //       (parseInt(this.countryData[i].TotalRecovered) +
+      //         parseInt(this.countryData[i].TotalDeaths))
+      //       }</div>
+      //      </ion-col>
+      //      <ion-col style='text-align: center; color: #238e43;'>
+      //      <div >Recovered</div>
 
-       <div >${this.countryData[i].TotalRecovered}</div>
-  <div style='font-size: 11px;'>+ [${this.countryData[i].NewRecovered}]</div>
-       </ion-col>
-       <ion-col style="text-align: center; color: #2f2f2f;"
-       >
-       <div>Deceased</div>
-        <div >${this.countryData[i].TotalDeaths}</div>
-        <div style='font-size: 11px;'>+ [${this.countryData[i].NewDeaths}]</div>
-       </ion-col>
-       </ion-row>
-     </ion-card-content>
-   </ion-card>
-       </ion-card>
-    `);
+      //      <div >${this.countryData[i].TotalRecovered}</div>
+      // <div style='font-size: 11px;'>+ [${this.countryData[i].NewRecovered}]</div>
+      //      </ion-col>
+      //      <ion-col style="text-align: center; color: #2f2f2f;"
+      //      >
+      //      <div>Deceased</div>
+      //       <div >${this.countryData[i].TotalDeaths}</div>
+      //       <div style='font-size: 11px;'>+ [${this.countryData[i].NewDeaths}]</div>
+      //      </ion-col>
+      //      </ion-row>
+      //    </ion-card-content>
+      //  </ion-card>
+      //      </ion-card>
+      //   `);
+      $('.loadingPieChatData').click((event) => {
+        const id = event.currentTarget.id;
+        this.loadPieChart(this.countryData[id]);
+      });
     }
+  }
+  loadPieChart(countryObject) {
+    const canvas = document.getElementById('world_data') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+    gradient.addColorStop(0, 'rgba(58,72,237,0.3)');
+    gradient.addColorStop(1, 'rgba(255,255,255,1)');
+    const labelsData = ['Confirmed', 'Recovered', 'Deceased'];
+    const datasetData = [countryObject.TotalConfirmed, countryObject.TotalRecovered, countryObject.TotalDeaths];
+    const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: labelsData,
+        datasets: [
+          {
+            label: 'Total Reported Cases',
+            data: datasetData,
+            backgroundColor: [
+              '#ff5959',
+              '#59ffa6',
+              '#e3e3e3'
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        maintainAspectRatio: false,
+        scales: {
+          animation: {
+            duration: 2000,
+            easing: 'easeOutQuart',
+          },
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    });
+    $('#blueback').text(countryObject.Country);
   }
 }
